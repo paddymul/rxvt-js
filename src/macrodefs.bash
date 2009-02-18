1d70e6f608cad15f44b10877c1f7a247001d0bbc
Patrick Mullen <patrickmullen@Macintosh-3.local>
2009-02-18 12:39:48 -0500

    script to count macrodefs, and their usages and point back to definitions seems to work properly

--- src/macrodefs.bash
+++ src/macrodefs.bash
@@ -1,4 +1,9 @@
-ls | grep "[.][Chx]" | grep -v js | xargs grep -h "#define" | gsed -r 's/#define ([a-zA-Z_]*).*/\1/' | sort -u > macrodefs
-cat macrodefs |xargs -L 1  -I {}  echo " echo \`cat screen.C command.C | grep -h -c  {}  \`\" {} \" \` ls | grep \"[.][Chx]\" | grep -v js | xargs grep  \"#define {}\" \` " > count_macrodefs
+ls |\
+grep "[.][Chx]" | grep -v js |\
+xargs grep -h "^#define" > raw_defs
+cat raw_defs | gsed -r 's/#define ([a-zA-Z_]*)[^a-zA-Z_]*.*/\1/' > cleaned_defs
+cat cleaned_defs | sort -u  > macrodefs
+cat macrodefs |\
+xargs -L 1   -I {}  echo " echo \`cat screen.C command.C | grep -h -c  {}[^a-zA-Z_]  \`\" {} \" \` ls | grep \"[.][Chx]\" | grep -v js | xargs grep  \"#define {}[^a-zA-Z_]\" \` " >count_macrodefs
 chmod +x count_macrodefs
-./count_macrodefs
+./count_macrodefs | sort -n 
