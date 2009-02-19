9c367465045b0cb184323749d2383430a3c7b977
Patrick Mullen <patrickmullen@Macintosh-3.local>
2009-02-18 23:00:04 -0500

    more miscelany

--- src/macrodefs.bash
+++ src/macrodefs.bash
@@ -6,4 +6,4 @@ cat cleaned_defs | sort -u  > macrodefs
 cat macrodefs |\
 xargs -L 1   -I {}  echo " echo \`cat screen.C command.C | grep -h -c  {}[^a-zA-Z_]  \`\" {} \" \` ls | grep \"[.][Chx]\" | grep -v js | xargs grep  \"#define {}[^a-zA-Z_]\" \` " >count_macrodefs
 chmod +x count_macrodefs
-./count_macrodefs | sort -n 
+./count_macrodefs | sort -n  > macro_usages
