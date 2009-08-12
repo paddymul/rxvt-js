6aa3b7478354716ef0a35f175770823a70a6f0c7
Patrick Mullen <patrickmullen@Macintosh-3.local>
2009-08-12 11:24:54 -0400

    now I can read in a file via rhino

--- src/test/spdr_test.js
+++ src/test/spdr_test.js
@@ -1,8 +1,16 @@
-
+/*
 $ = function(){}
 load("../test_expand_both.c.js")
-
 var a = new rxvt_term();
 a.scr_poweron();
-f = new File(arguments[0]);
-print(f);
+var input =[];
+*/
+//java -jar ~/temp/downloads/js/rhino/rhino1_6R7/js.jar spdr_test.js
+a=readFile("../command.C.js");
+for(var i =0; i <a.length; i++){
+    if(i%2==0){
+        print(a[i]);
+    }
+}
+//f = new File(arguments[0]);
+//print(f);
